1f4349e92fcf994ac5d7497d81b264a6118eb776
HoangDH <daohuyhoang9119@gmail.com>
2023-03-11 18:29:02 +0700

    Add setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,13 @@
-/* eslint-disable no-unused-vars */
-import { React } from "react";
 import { createRoot } from "react-dom/client";
 import Pet from "./Pet";
+import SearchParams from "./SearchParam";
 
-// const Pet = (props) => {
-//     return React.createElement("div", {}, [
-//         React.createElement("h1", {}, props.name),
-//         React.createElement("h2", {}, props.animal),
-//     ]);
-// };
-
-// const App = () => {
-//     return React.createElement("div", { className: "heading-tag" }, [
-//         React.createElement("h1", {}, "Adopt Me!"),
-//         React.createElement(Pet, {
-//             name: "T",
-//             animal: "Dog",
-//         }),
-//         React.createElement(Pet, {
-//             name: "N",
-//             animal: "Cat",
-//         }),
-//         React.createElement(Pet, {
-//             name: "K",
-//             animal: "Tiger",
-//         }),
-//     ]);
-// };
 const App = () => {
     return (
         <div>
             <h1>Adopt Me!</h1>
             <Pet name="Luna" animal="dog" breed="Havanese" />
-            <Pet name="Pepper" animal="bird" breed="Cockatiel" />
-            <Pet name="Doink" animal="cat" breed="Mix" />
+            <SearchParams />;
         </div>
     );
 };
