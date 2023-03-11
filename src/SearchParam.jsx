886a8d31a5490ab57d9869342e7ea85689b04a6c
HoangDH <daohuyhoang9119@gmail.com>
2023-03-11 23:58:24 +0700

    Update setState

--- src/SearchParam.jsx
+++ src/SearchParam.jsx
@@ -1,11 +1,18 @@
+import { useState } from "react";
+let counter = 0;
+
 const SearchParams = () => {
-    const location = "Seattle, WA";
+    counter++;
+    const [location, setLocation] = useState("Seattle, WA");
+    // const location = "Seattle, WA";
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
+                    {counter} <br />
                     Location
                     <input
+                        onChange={(e) => setLocation(e.target.value)}
                         id="location"
                         value={location}
                         placeholder="Location"
