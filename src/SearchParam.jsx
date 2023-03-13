6d0d514b9c8c5fa7732c6d3977464f9df185acf6
HoangDH <daohuyhoang9119@gmail.com>
2023-03-13 10:21:03 +0700

    update search param

--- src/SearchParam.jsx
+++ src/SearchParam.jsx
@@ -1,15 +1,17 @@
 import { useState } from "react";
-let counter = 0;
+
+const ANIMALS = ["dog", "cat", "rabbit", "zebra", "leopard"];
 
 const SearchParams = () => {
-    counter++;
     const [location, setLocation] = useState("Seattle, WA");
+    const [animal, setAnimal] = useState("");
+    const [breed, setBreed] = useState("");
+    const BREEDS = ["dsd", "saaaa"];
     // const location = "Seattle, WA";
     return (
         <div className="search-params">
             <form>
                 <label htmlFor="location">
-                    {counter} <br />
                     Location
                     <input
                         onChange={(e) => setLocation(e.target.value)}
@@ -17,6 +19,38 @@ const SearchParams = () => {
                         value={location}
                         placeholder="Location"
                     />
+                </label>
+                <label htmlFor="animal">
+                    Animal
+                    <select
+                        id="animal"
+                        // value={animal}
+                        onChange={(e) => {
+                            setAnimal(e.target.value);
+                            setBreed("");
+                        }}
+                    >
+                        <option />
+                        {ANIMALS.map((animal) => (
+                            <option key={animal}>{animal}</option>
+                        ))}
+                    </select>
+                </label>
+                <label htmlFor="breed">
+                    Breed
+                    <select
+                        id="breed"
+                        // value={animal}
+                        onChange={(e) => {
+                            setBreed(e.target.value);
+                        }}
+                        disabled={BREEDS.length === 0}
+                    >
+                        <option />
+                        {BREEDS.map((breed) => (
+                            <option key={breed}>{breed}</option>
+                        ))}
+                    </select>
                 </label>
                 <button>Submit</button>
             </form>
