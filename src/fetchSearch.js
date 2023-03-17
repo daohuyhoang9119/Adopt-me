372ad11c599c940444ed0c7444d42f945eb55636
HoangDH <daohuyhoang9119@gmail.com>
2023-03-17 16:40:54 +0700

    update reacquery breed, pet, search

--- src/fetchSearch.js
+++ src/fetchSearch.js
@@ -3,7 +3,7 @@ async function fetchSearch({ queryKey }) {
     const res = await fetch(
         `http://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
     );
-    if (!res.oke) {
+    if (!res.ok) {
         throw new Error(
             `pet search not okay: ${animal}, ${location}, ${breed}`
         );
